5f8cb169399cbd66e9cc99a9e0ce764c4f6138d7
Alexandre Chaussier <alexandre.chaussier@temelio.com>
2017-08-22 07:42:03 +0200

    Remove legacy files

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -7,9 +7,12 @@ from testinfra.utils.ansible_runner import AnsibleRunner
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_foo_a(User):
-    assert User().name == 'root'
+def test_install_on_debian_family(host):
+    """
+    Test package installation on Debian family
+    """
 
+    if host.system_info.distribution not in ['debian', 'ubuntu']:
+        pytest.skip('Not apply to %s' % host.system_info.distribution)
 
-def test_foo_b(User):
-    assert User().name == 'root'
+    assert host.package('python-pip').is_installed
